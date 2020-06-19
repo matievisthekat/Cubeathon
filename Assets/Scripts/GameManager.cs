61ebfd47c5c6ba7dc4a004b40d5e2c90921444d1
MatievisThKat <matievisthekat@gmail.com>
2020-06-19 18:51:44 +0200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameEnded = false;
+
+    public float deathScreenDelay = 1f;
+    public GameObject completeLevelUI;
+    public PlayerMovement player;
+    public void EndGame()
+    {
+        if (!gameEnded)
+        {
+            gameEnded = true;
+            Invoke("Restart", deathScreenDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+        // player.sidewaysForce = 0f;
+        player.jumpForce = 0f;
+        player.forwardForce = 0f;
+        player.rb.AddForce(0, 0, -100);
+    }
+}
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision colInfo)
+    {
+        if (colInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
