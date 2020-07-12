ba96bb1fbdc43520e7cc72ced54ee24223efa2a0
MatievisTheKat <matievisthekat@gmail.com>
2020-07-12 17:20:18 +0200

    add 2 more levels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,31 +3,39 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    bool gameEnded = false;
+  bool gameEnded = false;
 
-    public float deathScreenDelay = 1f;
-    public GameObject completeLevelUI;
-    public PlayerMovement player;
-    public void EndGame()
+  public float deathScreenDelay = 1f;
+  public GameObject completeLevelUI;
+  public PlayerMovement player;
+  public void EndGame()
+  {
+    if (!gameEnded)
     {
-        if (!gameEnded)
-        {
-            gameEnded = true;
-            Invoke("Restart", deathScreenDelay);
-        }
+      gameEnded = true;
+      Invoke("Restart", deathScreenDelay);
     }
+  }
 
-    void Restart()
+  public void Update()
+  {
+    if (Input.GetKey("r"))
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+      Restart();
     }
+  }
 
-    public void CompleteLevel()
-    {
-        completeLevelUI.SetActive(true);
-        // player.sidewaysForce = 0f;
-        player.jumpForce = 0f;
-        player.forwardForce = 0f;
-        player.rb.AddForce(0, 0, -100);
-    }
+  void Restart()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+
+  public void CompleteLevel()
+  {
+    completeLevelUI.SetActive(true);
+    // player.sidewaysForce = 0f;
+    player.jumpForce = 0f;
+    player.forwardForce = 0f;
+    player.rb.AddForce(0, 0, -100);
+  }
 }
